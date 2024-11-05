e728c4767b2b087bdea3490a368ac15f73fcd1db
Arnav Samal <samalarnav@gmail.com>
2024-11-05 18:10:57 +0530

    adding a2 & organizing code

--- assignments/2_assign/run.py
+++ assignments/2_assign/run.py
@@ -52,8 +52,8 @@ def train(parser, train_data, dev_data, output_path, batch_size=1024, n_epochs=1
     ### Please see the following docs for support:
     ###     Adam Optimizer: https://pytorch.org/docs/stable/optim.html
     ###     Cross Entropy Loss: https://pytorch.org/docs/stable/nn.html#crossentropyloss
-
-
+    optimizer = optim.Adam(parser.model.parameters(), lr=lr)
+    loss_func = nn.CrossEntropyLoss()
 
     ### END YOUR CODE
 
@@ -106,7 +106,17 @@ def train_for_epoch(parser, train_data, dev_data, optimizer, loss_func, batch_si
             ### Please see the following docs for support:
             ###     Optimizer Step: https://pytorch.org/docs/stable/optim.html#optimizer-step
 
+            # Forward pass: get logits by running train_x through the model
+            logits = parser.model(train_x)
 
+            # Compute the cross-entropy loss between logits and train_y
+            loss = loss_func(logits, train_y)
+
+            # Backpropagate the loss
+            loss.backward()
+
+            # Optimizer step: update the model parameters based on the computed gradients
+            optimizer.step()
 
 
             ### END YOUR CODE
